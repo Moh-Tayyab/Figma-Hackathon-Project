b78132186027e09fa0d591092a7de611481372c6
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-12-11 04:32:04 +0500

    navbar updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
             height={32}
           />
           <Link href="/">
-            <h1 className="text-3xl font-inter font-bold text-Text2 cursor-pointer lg:pr-20">
+            <h1 className="text-3xl font-inter font-bold text-Text2 cursor-pointer lg:pr-20 hover:text-[#f7e9d9]">
               Furniro
             </h1>
           </Link>
@@ -46,25 +46,25 @@ const Header = () => {
         >
           <Link
             href="#"
-            className="text-Text2 hover:underline font-normal font-poppins text-[16px]"
+            className="text-black hover:underline font-normal font-poppins text-[16px] hover:text-[#f7e9d9]"
           >
             Home
           </Link>
           <Link
             href="/shop"
-            className="text-Text2 hover:underline font-normal font-poppins text-[16px]"
+            className="text-black hover:underline font-normal font-poppins text-[16px] hover:text-[#f7e9d9]"
           >
             Shop
           </Link>
           <Link
             href="/blog"
-            className="text-Text2 hover:underline font-normal font-poppins text-[16px]"
+            className="text-black hover:underline font-normal font-poppins text-[16px] hover:text-[#f7e9d9]"
           >
             Blog
           </Link>
           <Link
             href="/contact"
-            className="text-Text2 hover:underline font-normal font-poppins text-[16px]"
+            className="text-black hover:underline font-normal font-poppins text-[16px] hover:text-[#f7e9d9]"
           >
             Contact
           </Link>
@@ -73,17 +73,17 @@ const Header = () => {
         {/* Search and Icons */}
         <div className="hidden lg:flex gap-8 items-center">
           <Link href={'/'}>
-            <TbUserExclamation className="h-[32px] w-[32px] hover:scale-110 hover:text-Button1" />
+            <TbUserExclamation className="h-[32px] w-[32px] hover:scale-110 hover:text-[#f7e9d9]" />
           </Link>
           <Link href={'/'}>
           
-            <IoSearch className="h-[32px] w-[32px] text-center hover:scale-110" />
+            <IoSearch className="h-[32px] w-[32px] text-center hover:scale-110 hover:text-[#f7e9d9]" />
           </Link>
           <Link href={'/checkout'}>
-            <FaRegHeart className="h-[32px] w-[32px] hover:scale-110 hover:text-Button2 " />
+            <FaRegHeart className="h-[32px] w-[32px] hover:scale-110  hover:text-[#f7e9d9]" />
           </Link>
           <Link href={'/cart'}>
-            <MdOutlineShoppingCart className="h-[32px] w-[32px] hover:scale-110 hover:text-Button2" />
+            <MdOutlineShoppingCart className="h-[32px] w-[32px] hover:scale-110 hover:text-[#f7e9d9]" />
           </Link>
         </div>
       </div>
